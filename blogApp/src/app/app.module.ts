4f0b529e16333bed405b272905b8889aaab8625d
CoderRayMan <ankshukray@gmail.com>
2023-07-26 16:38:01 +0530

    initial - strcuture

--- blogApp/src/app/app.module.ts
+++ blogApp/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatToolbarModule} from '@angular/material/toolbar'
+import{MatIconModule} from '@angular/material/icon';
+import { CardCarouselComponent } from './card-carousel/card-carousel.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardCarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
